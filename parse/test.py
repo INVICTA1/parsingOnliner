5ebb11c46212a30f4e64de38977148b957bcd2af
Maksim <makc.pinsk@gmail.com>
2020-09-10 17:32:22 +0300

    save to file csv

--- parse/test.py
+++ parse/test.py
@@ -1,89 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import regex as re
-
-
-def parse_storgame():
-    URL = 'https://stopgame.ru/topgames'
-    r = requests.get(URL)
-    html = BeautifulSoup(r.content, 'html.parser')
-    for elem in html.select('.game-summary-horiz'):
-        title = elem.select('.caption > a')
-        print(title[0].text)
-
-
-def parse_onliner():
-    URL = 'https://r.onliner.by/ak/?rent_type%5B%5D=room&only_owner=true#bounds%5Blb%5D%5Blat%5D=53.89129696150157&bounds%5Blb%5D%5Blong%5D=27.304834215169524&bounds%5Brt%5D%5Blat%5D=53.960988463831015&bounds%5Brt%5D%5Blong%5D=27.75315394037773'
-    HEADERS = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-        'params': '*/*'}
-    r = requests.get(URL, headers=HEADERS, params=None)
-
-    html = BeautifulSoup(r.content, 'html.parser')
-    print(html)
-    # for elem in html.select('.classified > a'):
-    #     print(elem)
-    # for elem in html.select('.classified'):
-    #     print(elem)
-    items = html.find_all('a', class_='classified')
-    # price = elem.select('.classified__price classified__price_secondary')
-    # time = elem.select('.classified__time > a')
-    # adress = elem.select('.classified__caption-item classified__caption-item_adress')
-    # print(time.text, price, adress)
-
-
-def selenium():
-    from selenium import webdriver
-    flats = []
-    URL = 'https://r.onliner.by/ak/'
-    chromedriver = 'resources\chromedriver.exe'
-    # def open_browser(chromedriver):
-    options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
-    options.add_argument('--incognito')
-    browser = webdriver.Chrome(executable_path=chromedriver, options=options)
-    browser.implicitly_wait(2)
-    browser.maximize_window()
-    # return browser
-    # browser = open_browser(chromedriver)
-    browser.get(URL)
-    requiredHtml = browser.page_source
-    soup = BeautifulSoup(requiredHtml, 'html.parser')
-
-    amount_page = soup.select('.pagination-pages__item')[-1].text
-
-    for page in range(1, int(amount_page) + 1):
-        print(f"Parse page {page} by {URL + '#page=' + str(page)}...")
-        # options.add_argument({"page": page})
-        browser.get(URL + '#page=' + str(page))
-        requiredHtml = browser.page_source
-        soup = BeautifulSoup(requiredHtml, 'html.parser')
-        for elem in soup.select('.classified'):
-            dollar_price = ''
-            rub_price = ''
-            price = elem.select('.classified__price-value > span')
-            time = elem.select('.classified__time')
-            address = elem.select('.classified__caption-item_adress')
-            for i in range(2):
-                dollar_price = dollar_price + str(price[i].text.strip())
-                rub_price = rub_price + str(price[i + 2].text.strip())
-            flats.extend([['Prise USD: ' + dollar_price, 'Prise BYN: ' + rub_price, 'time: ' + time[0].text.strip(),
-                           'address: ' + address[0].text.strip()]])
-            # print('Prise:', dollar_price,'or' ,rub_price, 'time:', time[0].text.strip(), 'address:', address[0].text.strip())
-
-    browser.quit()
-    for i in flats:
-        print(i)
-
-
-# selenium()
-'''Научится пробрасвыть на верх Exception и отлавливать его'''
-
+from selenium import webdriver
+import csv
 from datetime import datetime, timedelta
 
 
-def parse_date(date_time, time='2 дня назад'):
-    print(date_time)
+flats = []
+URL = 'https://r.onliner.by/ak/'
+chromedriver = r'..\resources\chromedriver.exe'
+
+
+def parse_date(date_time, time=None):
     data_time = {
         'секунду,секунды,секунд': 1,
         'минуту,минуты,минут': 60,
@@ -99,10 +27,78 @@ def parse_date(date_time, time='2 дня назад'):
                 exam = value
         if not exam:
             return Exception
-        return (date_time - timedelta(seconds=int(exam)*int(words[0]))).strftime("%Y-%m-%d %H:%M:%S")
+        print((date_time - timedelta(seconds=int(exam) * int(words[0]))).strftime("%Y-%m-%d %H:%M:%S"),type((date_time - timedelta(seconds=int(exam) * int(words[0]))).strftime("%Y-%m-%d %H:%M:%S")))
+        return (date_time - timedelta(seconds=int(exam) * int(words[0]))).strftime("%Y-%m-%d %H:%M:%S")
     except Exception:
-        print("wrong data")
-parse_date(datetime.now())
+        return 'Not data'
+    # parse_date(datetime.now())
+
+
+def open_browser(chromedriver):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('--incognito')
+    browser = webdriver.Chrome(executable_path=chromedriver, options=options)
+    browser.implicitly_wait(2)
+    browser.maximize_window()
+    return browser
+
+
+def get_page(browser, url):
+    browser.get(url)
+    requiredHtml = browser.page_source
+    soup = BeautifulSoup(requiredHtml, 'html.parser')
+    return soup
+
+
+def get_amount_pages(soup):
+    amount_page = soup.select('.pagination-pages__item')[-1].text
+    return amount_page
+
+
+def parse_other_pages(browser, amount_page):
+    for page in range(1, int(amount_page) + 1):
+        print(f"Parse page {page} by {URL + '#page=' + str(page)}...")
+        soup = get_page(browser, URL + '#page=' + str(page))
+        for elem in soup.select('.classified'):
+            dollar_price = ''
+            rub_price = ''
+            price = elem.select('.classified__price-value > span')
+            time = elem.select('.classified__time')
+            address = elem.select('.classified__caption-item_adress')
+            for i in range(2):
+                dollar_price = dollar_price + str(price[i].text.strip())
+                rub_price = rub_price + str(price[i + 2].text.strip())
+            flats.extend([['Prise USD: ' + dollar_price, 'Prise BYN: ' + rub_price,
+                           'time: ' , parse_date(datetime.now(), str(time[0].text.strip())),
+                           'address: ' + address[0].text.strip()]])
+    return flats
+
+
+def show_flats(flats):
+    for i in flats:
+        print(i)
+
+
+def save_result_csv(flats):
+    path = r'..\resources\test.csv'
+    with open(path, 'w') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+        for flat in flats:
+            writer.writerow(flat)
+
+
+def main():
+    browser = open_browser(chromedriver)
+    soup = get_page(browser, URL)
+    amount_page = get_amount_pages(soup)
+    flats = parse_other_pages(browser, amount_page)
+    browser.quit()
+    show_flats(flats)
+    save_result_csv(flats)
+
+
+main()
 
 # if __name__ == '__test__':
 #     pass
