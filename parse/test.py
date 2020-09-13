0b3efa29ef8adaf79f6019d945f9cb2074fcfd67
Maksim <makc.pinsk@gmail.com>
2020-09-13 12:04:49 +0300

    added criteria

--- parse/test.py
+++ parse/test.py
@@ -4,18 +4,12 @@ import regex as re
 from selenium import webdriver
 import csv
 from datetime import datetime, timedelta
+import os
 
 flats = []
 
 
-def parse_date(date_now, time=None):
-    data_time = {
-        'секунду,секунды,секунд': 1,
-        'минуту,минуты,минут': 60,
-        'час,часов,часа': 3600,
-        'день,дня,дней': 86400,
-        'неделя,недели,недель': 604800,
-    }
+def parse_date(date_now, data_time, time=None):
     exam = ''
     try:
         words = time.split(' ')
@@ -23,8 +17,8 @@ def parse_date(date_now, time=None):
             if re.search(words[1].strip(), key):
                 exam = value
         if not exam:
-            return Exception
-        if time.strip()=='час назад':
+            return 'Not data'
+        if time.strip() == 'час назад':
             return (date_now - timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
         else:
             return (date_now - timedelta(seconds=int(exam) * int(words[0]))).strftime("%Y-%m-%d %H:%M:%S")
@@ -54,41 +48,50 @@ def get_amount_pages(soup):
     return amount_page
 
 
-def parse_other_pages(browser, amount_page, url):
+def parse_other_pages(browser, amount_page, url, data_time):
     for page in range(1, int(amount_page) + 1):
         print(f"Parse page {page} by {url + '#page=' + str(page)}...")
         soup = get_page(browser, url + '#page=' + str(page))
         for elem in soup.select('.classified'):
             dollar_price = ''
             rub_price = ''
+            rooms = elem.select('.classified__caption-item_type')
             price = elem.select('.classified__price-value > span')
             time = elem.select('.classified__time')
             address = elem.select('.classified__caption-item_adress')
+            link = elem.get('href')
             for i in range(2):
                 dollar_price = dollar_price + str(price[i].text.strip())
                 rub_price = rub_price + str(price[i + 2].text.strip())
-            flats.extend([['Prise USD: ' + dollar_price, 'Prise BYN: ' + rub_price,
-                           'time: ', parse_date(datetime.now(), str(time[0].text.strip())),
-                           'address: ' + address[0].text.strip()]])
+            flats.extend([(dollar_price , rub_price ,
+                           parse_date(datetime.now(), data_time, str(time[0].text.strip())),
+                           address[0].text.strip(), rooms[0].text.strip(), link)])
     return flats
 
 
 def save_result_csv(flats):
     path = r'..\resources\test.csv'
-    with open(path, 'w') as file:
+    with open(path, 'w', encoding='cp1251',  newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Prise USD: ', 'Prise BYN: ', 'Time: ', 'Address: ', 'room: ', 'link: '])
         for flat in flats:
             writer.writerow(flat)
 
 
 def parseOnliner():
     URL = 'https://r.onliner.by/ak/'
-    chromedriver = r'..\resources\chromedriver.exe'
+    chromedriver = os.environ.get('Chromedriver_path')
+    data_time = {
+        'секунду,секунды,секунд': 1,
+        'минуту,минуты,минут': 60,
+        'час,часов,часа': 3600,
+        'день,дня,дней': 86400,
+        'неделя,недели,недель': 604800,
+    }
     browser = open_browser(chromedriver)
     soup = get_page(browser, URL)
     amount_page = get_amount_pages(soup)
-    flats = parse_other_pages(browser, amount_page, URL)
-    print(flats)
+    flats = parse_other_pages(browser, amount_page, URL, data_time)
     browser.quit()
     save_result_csv(flats)
 
